d38f8f1a05dff3c58b11bb43b7a2c1f6f95aa383
aakash2016 <akash.gagrawal@gmail.com>
2024-10-25 00:34:57 -0700

    add selector

--- main.py
+++ main.py
@@ -12,7 +12,14 @@ def main():
     option = st.sidebar.radio("Select a simulation:", ('casino', 'poker', 'monte_hall', 'insurance_claims'))
 
     if option == 'casino':
-        remain_funds = simulate_casino(10000, 100, 1000)
+        form = st.form("Parameters")
+        total_funds = form.slider(label='Total money in hand', min_value=500, max_value=20000, value=2000)
+        wager_amount = form.slider(label='Enter the betting amount', min_value=500, max_value=total_funds, value=1000)
+        total_plays = form.slider(label='Number of games', min_value=1, max_value=100, value=10)
+        form.form_submit_button("submit")
+
+        # run simulation
+        remain_funds = simulate_casino(total_funds, wager_amount, total_plays)
         st.write(f"{remain_funds}")
     elif option == 'Poker':
         pass
