9313d732c9a30ef83149458186b4a30193a5b7f2
Rishabh Thapliyal <rishabh@Rishabhs-MacBook-Air.local>
2024-11-09 14:19:33 -0800

    compiled together

---
+++ concepts/intro.py
@@ -0,0 +1,26 @@
+def intro():
+    import streamlit as st
+
+
+    st.markdown("<h1 style='text-align: center; color: black;'>Course Project 💻</h1>", unsafe_allow_html=True)
+    # st.title("Course Project! :computer:")
+    st.write("#### ECE 225: Probability and Statistics for Data Science! :chart_with_upwards_trend:")
+
+    st.sidebar.success("Select a demo above.")
+
+    st.markdown(
+        """
+        We aim to demonstrate key class concepts through interactive examples, making complex ideas easier to understand and interpret.
+
+        ### What will we cover?
+
+        - Sum of Two Gaussians: Showing how the sum of two Gaussian distributions results in another Gaussian.
+        - Central Limit Theorem: Illustrating how this fundamental statistical principle works in practice.
+        - Poisson Distribution as a Special Case of Binomial Distribution: Demonstrating how the Poisson distribution emerges from the binomial distribution under certain conditions.
+        
+        ### Team members
+
+        - Rishabh Thapliyal
+        - Aakash Agarwal
+    """
+    )
---
+++ concepts/main.py
@@ -0,0 +1,15 @@
+import streamlit as st
+from intro import intro
+from gaussian import plot_sum_gaussian
+from clt import plot_clt
+from binomial_to_poisson import plot_binomial_to_poisson
+
+page_names_to_funcs = {
+    "—": intro,
+    "Sum of 2 gaussians": plot_sum_gaussian,
+    "Central Limit Theorem": plot_clt,
+    "Binomial-poisson": plot_binomial_to_poisson
+}
+
+demo_name = st.sidebar.selectbox("Choose a demo", page_names_to_funcs.keys())
+page_names_to_funcs[demo_name]()
